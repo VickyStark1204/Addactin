2d31d9a2331912b8c128e0229732e594c4035d00
VickyStark1204 <cvignesh124@gmail.com>
2021-10-08 21:20:36 +0530

    New File Added

---
+++ src/test/java/org/Fb/NewRepo.java
@@ -0,0 +1,11 @@
+package org.Fb;
+
+public class NewRepo {
+
+	public static void username() {
+		System.out.println("Void Main");
+	}
+	public static void main(String[] args) {
+		username();
+	}
+}
